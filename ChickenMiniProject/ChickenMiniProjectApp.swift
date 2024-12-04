050a05084ce02ff3f209de6ef02cdca1b63786f0
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-12-04 12:28:19 +0700

    Initial Commit

---
+++ ChickenMiniProject/ChickenMiniProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenMiniProjectApp.swift
+//  ChickenMiniProject
+//
+//  Created by Lonard Steven on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChickenMiniProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickenMiniProjectTests/ChickenMiniProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenMiniProjectTests.swift
+//  ChickenMiniProjectTests
+//
+//  Created by Lonard Steven on 04/12/24.
+//
+
+import Testing
+@testable import ChickenMiniProject
+
+struct ChickenMiniProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
