0f2c33bd13f3ad3a58cb38cc9f3967fe75609a60
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-12-08 15:11:13 +0700

    fix: Making grids aren't having weird spacing

--- ChickenMiniProject/View/ContentView.swift
+++ ChickenMiniProject/View/ContentView.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class ContentView: UIViewController {
+class ContentView: UIViewController, GridViewControllerDelegate, UISearchBarDelegate {
     
     // store Meals data
     var meals: [Meal] = []
+    // store filter/area categories
+    var filterCategories: [String] = []
+    
+    private var gridViewController: GridViewController!
     
     lazy var titleBar: UILabel = {
         let label = UILabel()
@@ -28,6 +32,8 @@ class ContentView: UIViewController {
         searchBar.searchTextField.textColor = .label
         searchBar.searchTextField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         searchBar.setImage(UIImage(systemName: "magnifyingglass"), for: .search, state: .normal)
+        
+        searchBar.delegate = self
         
         return searchBar
     }()
@@ -55,7 +61,8 @@ class ContentView: UIViewController {
     }()
     
     private func addGridViewController() {
-        let gridViewController = GridViewController()
+        gridViewController = GridViewController()
+        gridViewController.delegate = self
         addChild(gridViewController)
         
         gridContainerView.addSubview(gridViewController.view)
@@ -68,6 +75,67 @@ class ContentView: UIViewController {
         gridViewController.didMove(toParent: self)
     }
     
+    private func setupFilterButtons(with categories: [String]) {
+        horizontalFilterStackView.arrangedSubviews.forEach { $0.removeFromSuperview() } // clear existing buttons
+        
+        for area in categories {
+            let button = UIButton(type: .system)
+            button.setTitle(area, for: .normal)
+            button.setTitleColor(.label, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+            button.backgroundColor = .systemBackground
+            button.layer.cornerRadius = 8
+            
+            button.addTarget(self, action: #selector(filterButtonTapped(_:)), for: .touchUpInside)
+            
+            horizontalFilterStackView.addArrangedSubview(button)
+        }
+    }
+    
+    @objc func filterButtonTapped(_ sender: UIButton) {
+        if let title = sender.title(for: .normal) {
+            filterMeals(by: title)
+        } else {
+            resetFilter()
+        }
+    }
+    
+    private func setupDismissKeyboardGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func resetFilter() {
+        gridViewController.resetFilter()
+    }
+
+    func filterMeals(by area: String) {
+        gridViewController.filterMeals(by: area)
+    }
+    
+    func fetchMealsAndSetupFilters() {
+        APIHelper.shared.fetchMeals { [weak self] result in
+            switch result {
+            case .success(let meals):
+                self?.meals = meals
+                self?.setupFilterButtons(with: APIHelper.shared.filterCategories)
+                
+                // pass to GridViewController
+                self?.gridViewController.allMeals = meals
+                self?.gridViewController.meals = meals
+                self?.gridViewController.reloadCollectionView()
+                
+            case .failure(let error):
+                print("Error while fetching the meals data: \(error.localizedDescription)")
+            }
+        }
+    }
+    
     
     override func loadView() {
         super.loadView()
@@ -75,34 +143,38 @@ class ContentView: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(titleBar)
         view.addSubview(searchBarView)
-        view.addSubview(horizontalFilterStackView)
+        view.addSubview(horizontalFilterScrollView)
+        view.addSubview(gridContainerView)
         titleBar.translatesAutoresizingMaskIntoConstraints = false
-        titleBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        titleBar.bottomAnchor.constraint(equalTo: searchBarView.topAnchor).isActive = true
-        titleBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        titleBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        titleBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        titleBar.bottomAnchor.constraint(equalTo: searchBarView.topAnchor, constant: -16).isActive = true
+        titleBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        titleBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
         
         searchBarView.translatesAutoresizingMaskIntoConstraints = false
-        searchBarView.topAnchor.constraint(equalTo: titleBar.bottomAnchor).isActive = true
-        searchBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        searchBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        searchBarView.bottomAnchor.constraint(equalTo: horizontalFilterStackView.topAnchor).isActive = true
+        searchBarView.topAnchor.constraint(equalTo: titleBar.bottomAnchor, constant: 16).isActive = true
+        searchBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        searchBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -16).isActive = true
     
-        horizontalFilterStackView.translatesAutoresizingMaskIntoConstraints = false
-        horizontalFilterStackView.topAnchor.constraint(equalTo: searchBarView.bottomAnchor).isActive = true
-        horizontalFilterStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        horizontalFilterStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        horizontalFilterStackView.bottomAnchor.constraint(equalTo: gridContainerView.topAnchor).isActive = true
+        horizontalFilterScrollView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalFilterScrollView.topAnchor.constraint(equalTo: searchBarView.bottomAnchor, constant: 16).isActive = true
+        horizontalFilterScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
         
         gridContainerView.translatesAutoresizingMaskIntoConstraints = false
-        gridContainerView.topAnchor.constraint(equalTo: horizontalFilterStackView.bottomAnchor).isActive = true
-        gridContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        gridContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        gridContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        gridContainerView.topAnchor.constraint(equalTo: horizontalFilterStackView.bottomAnchor, constant: 16).isActive = true
+        gridContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        gridContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        gridContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16).isActive = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        addGridViewController()
+        
+        fetchMealsAndSetupFilters()
+        setupDismissKeyboardGesture()
     }
+    
+    
 }
