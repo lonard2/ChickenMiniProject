c4cd00671c6e8416cbaf68dcdc646d4bc84c755b
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-12-08 18:57:46 +0700

    feat: Add padded button to accomodate a button's label padding

---
+++ ChickenMiniProject/View/PaddedButton.swift
@@ -0,0 +1,39 @@
+//
+//  PaddedButton.swift
+//  ChickenMiniProject
+//
+//  Created by Lonard Steven on 08/12/24.
+//
+
+import UIKit
+
+class PaddedButton: UIButton {
+    var titlePadding: UIEdgeInsets = .zero
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLabel?.sizeToFit()
+        
+        guard let titleLabel = titleLabel else { return }
+        
+        // title frame adjustment (padding)
+        let buttonWidth = titleLabel.intrinsicContentSize.width + titlePadding.left + titlePadding.right
+        let buttonHeight = titleLabel.intrinsicContentSize.height + titlePadding.top + titlePadding.bottom
+        
+        // set button frame size
+        self.frame.size = CGSize(width: buttonWidth, height: buttonHeight)
+        
+        // new position of titleLabel position
+        let titleX = titlePadding.left
+        let titleY = titlePadding.top
+        
+        titleLabel.frame = CGRect(x: titleX,
+                                   y: titleY,
+                                   width: buttonWidth - titlePadding.left - titlePadding.right,
+                                   height: titleLabel.frame.height)
+        
+        titleLabel.center.y = buttonHeight / 2
+        titleLabel.textAlignment = .center
+    }
+}
