a9206057acee96a114742bf0849245e3f9fac0a0
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-12-08 15:07:34 +0700

    fix: Revise the AppDelegate & SceneDelegate to ensure app could be run independently from SwiftUI

--- ChickenMiniProject/SceneDelegate.swift
+++ ChickenMiniProject/SceneDelegate.swift
@@ -11,16 +11,16 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        
-        let contentView = ContentView() // main menu
-        let navigationController = UINavigationController(rootViewController: contentView)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
+        let rootViewController = ContentView()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
+        print("Scene setup completed.")
     }
 }
-
